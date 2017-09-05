df1ca6ea7f21a5fd17a273caec68ebd79078768c
sph2116 <31591569+sph2116@users.noreply.github.com>
2017-09-05 19:46:45 -0400

    hw1 upload

---
+++ hw1_self_portrait/sketch.js
@@ -0,0 +1,79 @@
+function setup(){
+    createCanvas(800,800);
+    
+}
+
+function draw(){
+    background(236,218,218);
+    fill(247,167,173,100);
+    stroke(255);
+    strokeWeight(1);
+    ellipse(400,300,100,100);
+
+    fill(245,142,144,100);
+    stroke(255);
+    strokeWeight(1);
+    ellipse(400,300,125,125);
+    
+    fill(243,117,117,100);
+    stroke(255);
+    strokeWeight(1);
+    ellipse(400,300,190,190);
+    
+     fill(242,103,105,100);
+    stroke(255);
+    strokeWeight(1);
+    ellipse(400,300,250,250);
+    
+    /*Triangles*/
+    
+    fill(242,103,105,100);
+    stroke(182,172,221);
+    strokeWeight(2);
+    triangle(375,100,425,100,400,25);
+
+    
+
+    fill(242,103,105,100);
+    stroke(182,172,221);
+    strokeWeight(2);
+    triangle(275,150,325,150,300,75);
+    
+
+    
+    
+    fill(242,103,105,100);
+    stroke(182,172,221);
+    strokeWeight(2);
+    triangle(475,150,525,150,500,75);
+    
+    
+    /*triangles copy */
+    
+      translate(0,310);
+      fill(242,103,105,100);
+    stroke(182,172,221);
+    strokeWeight(2);
+    triangle(375,200,425,200,400,275);
+  
+
+    
+
+    fill(242,103,105,100);
+    stroke(182,172,221);
+    strokeWeight(2);
+    triangle(275,150,325,150,300,225);
+    
+
+    
+    
+    fill(242,103,105,100);
+    stroke(182,172,221);
+    strokeWeight(2);
+    triangle(475,150,525,150,500,225);
+    
+       
+    
+       
+ 
+}
